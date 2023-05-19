62a2e05e7de9e2f71ebe74041f742ce0bb00d6e5
Darkside2010 <133535466+Darkside2010@users.noreply.github.com>
2023-05-18 21:13:10 -0500

    Add files via upload

---
+++ sketch_mar28a-blouthoo.ino
@@ -0,0 +1,23 @@
+int estado=0;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+
+void loop(){
+ if(Serial.available()>0){
+ estado = Serial.read();
+ }
+  if (estado =='1'){
+   digitalWrite(13,HIGH);
+  }
+if(estado=='2'){
+   digitalWrite(13,LOW);
+  }
+  char X = Serial.read();
+  if(X=='a')
+    digitalWrite(13,HIGH);
+  if(X=='b')
+    digitalWrite(13,LOW);  
+}
